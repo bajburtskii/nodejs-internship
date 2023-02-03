cb49da48a3810ee656eb4b739bbf59e18be7a481
arcadij <arcadij@sportysystems.com>
2023-02-03 15:25:50 +0200

    task31

---
+++ lesson3/task21.js
@@ -0,0 +1,13 @@
+/**
+ * Write a JavaScript program to convert temperatures to and from celsius, fahrenheit.
+ *
+ * C = (5/9) * (F - 32)
+ *
+ * The results from C to F have to be available in Global Scope, and results from F to C have to be available only in a local scope.
+ *
+ * Please support your code with detailed comments answering the question `Why`, rather than `What`.
+ *
+ * Sample Output:
+ *    60°C is 140 °F.
+ *    45°F is 7.222222222222222°C.
+ */
