8891f6482eea3c79bdcae2e17c71b250d6d34bd6
DavidPark <Churkindava@gmail.com>
2023-02-04 11:13:41 +0200

    homework

---
+++ homework2/lesson_homework.js
@@ -0,0 +1,12 @@
+/* Converting from degrees Celsius to Fahrenheit is degrees */
+var gradC = 60;
+var gradF = ((gradC * 9 / 5) + 32);
+console.log("Converting from 60 degrees Celsius to Fahrenheit is " + gradF + " degrees");
+{
+    /*
+    The conversion from degrees Fahrenheit to Сelsius is degrees
+    */
+    let gradF = 45;
+    let gradC = (5 / 9) * (gradF - 32);
+    console.log("The conversion from 45 degrees fahrenheit to celsius is " + gradC + " degrees")
+}
