2997e805f02c63e29a2e666c05cf153dd5bdc813
vova <bordiian.v@gmail.com>
2023-02-06 14:52:24 +0200

    added another way to solve exercise with loop

--- lesson3/task31.js
+++ lesson3/task31.js
@@ -15,6 +15,39 @@ const isPalindrome = function(s) {
   return scrapedString === reversedString
 };
 
-console.log(isPalindrome('Hello World')) // false
-console.log(isPalindrome('12wow21')) // true  
-console.log(isPalindrome('1&2w^ow_%21#')) // true
+const isPalindromeLoop = function(s) {
+  const scrapedString = s.toLowerCase().replace(/[^a-z0-9 *]/g, '') // Modified regex not replacing spaces for exercise purposes
+  let result = true
+  let leftSpaceCount = 0 // Made for constructing indexes with ignoring spaces. Will be working properly only with single space
+  let rightSpaceCount = 0
+  for (let i = 0; i < Math.floor(scrapedString.length/2); i++) { // Loop ends before middle element if string length is odd
+    if (scrapedString.charAt(i) === ' ') leftSpaceCount++
+    if (scrapedString.charAt(scrapedString.length - i - 1) === ' ') rightSpaceCount++   
+    const leftIndex = i + leftSpaceCount
+    const rightIndex = scrapedString.length - i - 1 - rightSpaceCount
+    // In case when string has many single spaces don't allow loop to continue compare through elements in reverse
+    if (rightIndex <= leftIndex) {
+      break
+    // Also prevent wrong result in case with even string length that has space or spaces in middle
+    } else if ((scrapedString.charAt(leftIndex) !== scrapedString.charAt(rightIndex)) 
+      && ((scrapedString.charAt(rightIndex) !== ' ') && (scrapedString.charAt(leftIndex) !== ' '))) {
+      result = false
+      break
+    }
+  }
+  return result
+}
+
+
+// console.log(isPalindrome('Hello World')) //false
+// console.log(isPalindrome('12wow21')) // true
+// console.log(isPalindrome('1&2w^ow_%21#')) //true
+
+console.log(isPalindromeLoop('Hello World')) //false
+console.log(isPalindromeLoop('12wow21'))  // true
+console.log(isPalindromeLoop('1&2w^ow_%21#')) // true
+console.log(isPalindromeLoop('r ac a r')) // true
+console.log(isPalindromeLoop('race a car')) //false
+
+
+
