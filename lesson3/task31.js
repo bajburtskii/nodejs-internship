dbdfed6c8448c7034d434241d20bdddc62ee7d76
Prosta4ok_ua <danyavdele@gmail.com>
2023-02-07 01:25:28 +0200

    task 2 and task 3 are completed

--- lesson3/task31.js
+++ lesson3/task31.js
@@ -4,11 +4,21 @@
  *
  * Given a string s, return true if it is a palindrome, or false otherwise.
  */
-
+"use strict"
 /**
  * @param {string} s
  * @return {boolean}
  */
 const isPalindrome = function(s) {
-
+    // g - special flag that indicates Global search
+    s = s.replace(/[0-9]/g, '').toLowerCase();
+    console.log(s.split(""))
+    // "abc".split("") - output: ["a", "b", "c"]
+    return s.split("").reverse().toString() === s.split("").toString();
 };
+
+function tests() {
+    console.log(isPalindrome("2423432b"));
+}
+
+tests();
