d3952101059dec7ccf8817a411e32a4a812e305f
DavidPark <Churkindava@gmail.com>
2023-02-06 23:33:59 +0200

    Merge remote-tracking branch 'origin/homeworkChurkin' into homeworkChurkin

--- homework2/lesson_homework.js
+++ homework2/lesson_homework.js
@@ -1,4 +1,5 @@
-{/* Converting from degrees Celsius to Fahrenheit is degrees */
+{
+    /* Converting from degrees Celsius to Fahrenheit is degrees */
     let gradC = 60;
     let gradF = ((gradC * 9 / 5) + 32);
     console.log("Converting from 60 degrees Celsius to Fahrenheit is " + gradF + " degrees");
