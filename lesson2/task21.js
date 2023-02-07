70745c0fd33000d525f0419686e755e3d39d3fe8
vova <bordiian.v@gmail.com>
2023-02-07 12:55:57 +0200

    task21 completed

--- lesson2/task21.js
+++ lesson2/task21.js
@@ -11,3 +11,28 @@
  *    60°C is 140 °F.
  *    45°F is 7.222222222222222°C.
  */
+
+/**
+ * 
+ * @param {number} degree 
+ * @param {string} scaleType examples: "c" or "f"
+ * @returns {number}
+ */
+
+const convertTemp = (degree, scaleType) => {
+  switch (scaleType) {
+    case 'c':
+      const resultCels = 5/9 * (degree - 32)
+      console.log(`${degree}°F is ${resultCels}°C.`) // Don't return because of exercise purposes
+      return null
+    case 'f':
+      const resultFahr =  degree * 9/5 + 32
+      return `${degree}°C is ${resultFahr}°F.` 
+    default:
+      return 'Enter wanted degree scale ("f" / "c") PROPERLY'
+  }
+}
+
+const result = convertTemp(100, 'c')
+result ? console.log(result) : console.log('Haven\'t access from global scope to resultCels')
+
