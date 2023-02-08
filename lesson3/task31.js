3067db24606b8528ceccea2af6422ad240806cea
Artem Medvedev <a95.medvedev@gmail.com>
2023-02-08 17:56:34 +0200

    Task 31 solution

--- lesson3/task31.js
+++ lesson3/task31.js
@@ -6,9 +6,24 @@
  */
 
 /**
+ * Validates is given value is actually a string.
+ * @param {string} s - The current value.
+ */
+const isStr = s => typeof s === 'string' && isNaN(s);
+
+/**
  * @param {string} s
  * @return {boolean}
  */
 const isPalindrome = function(s) {
-
+    if (!isStr(s) || s === '') {
+        return 'Please enter a valid string.';
+    }
+    let clearString = s.toLowerCase().replace(/[\W_]/g, '');
+    let reversedString = clearString.split('').reverse().join('');
+    return reversedString === clearString;
 };
+
+console.log(isPalindrome(222)); // Output: Please enter a valid string.
+console.log(isPalindrome('')); // Output: Please enter a valid string.
+console.log(isPalindrome('A man, a plan, a canal. Panama;')); // Output: true
