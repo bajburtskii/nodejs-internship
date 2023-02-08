2beb9cac9dd0a70bc1039dc7c2525816584c9fa7
AbyssWalker1990 <bordiian.v@gmail.com>
2023-02-08 20:50:07 +0200

    refactored for better readability

--- lesson3/task32.js
+++ lesson3/task32.js
@@ -22,17 +22,12 @@ const longestCommonPrefix = function(strs) {
   let pref = ''
   if (Array.isArray(strs) && strs.length >= 1) {
     pref = strs.sort((a, b) => a.length - b.length)[0] // Shortest word can be prefix
-      for (let word of strs) {
-        while (pref.length >= 1) {
-          if (!word.startsWith(pref)) {
-            pref = pref.slice(0, -1)
-          } else {
-            break
-          }
-        }
+    for (let word of strs) {
+      while (pref.length >= 1) {
+        if (word.startsWith(pref)) break
+        pref = pref.slice(0, -1)         
       }
-  } else {
-    pref = ''
+    }
   }
   return pref
 };
