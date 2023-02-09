a263e5cb7a2b06ef3cedbb539c0376928476b012
Artem Medvedev <a95.medvedev@gmail.com>
2023-02-09 16:53:47 +0200

    Task 31 new solution

--- lesson3/task31.js
+++ lesson3/task31.js
@@ -6,24 +6,49 @@
  */
 
 /**
- * Validates is given value is actually a string.
- * @param {string} s - The current value.
- */
-const isStr = s => typeof s === 'string' && isNaN(s);
-
-/**
  * @param {string} s
  * @return {boolean}
  */
-const isPalindrome = function(s) {
-    if (!isStr(s) || s === '') {
+/* const isPalindrome = function(s) {
+    if (s === '') {
         return 'Please enter a valid string.';
     }
     let clearString = s.toLowerCase().replace(/[\W_]/g, '');
     let reversedString = clearString.split('').reverse().join('');
     return reversedString === clearString;
 };
-
 console.log(isPalindrome(222)); // Output: Please enter a valid string.
 console.log(isPalindrome('')); // Output: Please enter a valid string.
+console.log(isPalindrome('A man, a plan, a canal. Panama;')); // Output: true  */
+
+
+const isPalindrome = function(s) {
+    let str = s.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, ""), //Remove all special chars except spaces
+        start = 0,
+        end =  str.length - 1;
+
+    while (start < end) {
+        if (str.charAt(start) === ' ') {
+            start++;
+            continue;
+        }
+        if (str.charAt(end) === ' ') {
+            end--;
+            continue;
+        }
+
+        if (str.charAt(start) !== str.charAt(end)) {
+            return false;
+        }
+        start++;
+        end--;
+    }
+        return true;
+};
+
+console.log(isPalindrome("Madam i'm adam")); // Output: true
 console.log(isPalindrome('A man, a plan, a canal. Panama;')); // Output: true
+console.log(isPalindrome("Mmdam i'm adam")); // Output: false
+
+
+
