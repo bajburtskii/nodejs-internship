228631b5131a024ffc0e6da60eab1a1d63221f13
DavidPark <Churkindava@gmail.com>
2023-02-17 21:02:34 +0200

    Delete task21.js

--- lesson5/task51.js
+++ lesson5/task51.js
@@ -30,13 +30,13 @@ let symbol2 = "(]"
 
 const isValid = function(s) {
         const leftSymbols = [];
-        // Loop for each character of the string
+
         for (let i = 0; i < s.length; i++) {
-            // If left symbol is encountered
+
             if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
                 leftSymbols.push(s[i]);
             }
-            // If right symbol is encountered
+
             else if (s[i] === ')' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length - 1] === '(') {
                 leftSymbols.pop();
             } else if (s[i] === '}' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length - 1] === '{') {
@@ -44,7 +44,7 @@ const isValid = function(s) {
             } else if (s[i] === ']' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length - 1] === '[') {
                 leftSymbols.pop();
             }
-            // If none of the valid symbols is encountered
+
             else {
                 return false;
             }
