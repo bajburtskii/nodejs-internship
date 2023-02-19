38f6eaf6b6d7c4fcadd5130754bedf765f1a6ef0
Vova <bordiian.v@gmail.com>
2023-02-19 16:05:18 +0200

    task53 completed

---
+++ lesson5/task53.js
@@ -0,0 +1,40 @@
+/**
+ * Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+ *
+ * @example
+ *    Input: nums = [3,0,1]
+ *    Output: 2
+ *    Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+ *
+ * @example
+ *    Input: nums = [0,1]
+ *    Output: 2
+ *    Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+ *
+ * @example
+ *    Input: nums = [9,6,4,2,3,5,7,0,1]
+ *    Output: 8
+ *    Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+ *
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ const missingNumber = function(nums) {
+    if (!Array.isArray(nums)) return 'Invalid type object'
+    const n = nums.length
+    const isWrongType = nums.filter((num) => !Number.isInteger(num))
+    if (isWrongType.length > 0) return 'Invalid type in array'
+
+    const fullArray = [...Array(n+1).keys()]
+    const result = fullArray.filter((num) => !nums.includes(num))
+    return result[0]
+  };
+  
+  console.log(missingNumber([3,0,1]))
+  console.log(missingNumber([0,1]))
+  console.log(missingNumber([9,6,4,2,3,5,7,0,1]))
+  console.log(missingNumber('hello')) //Invalid type object
+  console.log(missingNumber(['1', '5', true])) // Invalid type in array
