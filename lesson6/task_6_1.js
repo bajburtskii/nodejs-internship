e68b473dfdf55c4c31fdda4a7c1dbac80df827a2
DavidPark <Churkindava@gmail.com>
2023-02-20 21:40:02 +0200

    Merge remote-tracking branch 'origin/lesson6' into lesson6

--- lesson6/task_6_1.js
+++ lesson6/task_6_1.js
@@ -1,1 +1,23 @@
-asder
+let items1 = [[1,1],[4,5],[3,8]];
+let items2 = [[3,1],[1,5]];
+const mergeSimilarItems = function(items1, items2) {
+    let answer = [];
+    let map = new Map();
+
+    for (let i = 0; i < items1.length; i++) {
+        map.set(items1[i][0], items1[i][1]);
+    }
+
+    for (let i = 0; i < items2.length; i++) {
+        map.set(items2[i][0], map.get(items2[i][0]) + items2[i][1] || items2[i][1]);
+    }
+
+    for (let [key, value] of map) {
+        answer.push([key, value]);
+    }
+
+    answer.sort((a, b) => a[0] - b[0]);
+    return answer;
+};
+
+console.log(mergeSimilarItems(items1,items2))
