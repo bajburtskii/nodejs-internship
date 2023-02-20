50cea2efcd3e71ce24fad7e9961c8d5ad04cfc34
Artem Medvedev <a95.medvedev@gmail.com>
2023-02-20 11:50:38 +0200

    Local changes

--- lesson3/task31.js
+++ lesson3/task31.js
@@ -10,19 +10,5 @@
  * @return {boolean}
  */
 const isPalindrome = function(s) {
-    s = s.replace(" ", "");
-    for (let index = 0; index < Math.floor(s.length) / 2; index++){
-        if (s[index] !== s[s.length - index - 1])
-            return false;
-    }
-    return true;
+
 };
-
-function tests() {
-    console.log(isPalindrome("2423432b"));          // Output: true
-    console.log(isPalindrome("abbbbbba"));          // Output: true
-    console.log(isPalindrome("abbcbbbbba"));        // Output: false
-    console.log(isPalindrome("6356354643"));        // Output: true
-}
-
-tests();
--- lesson3/task32.js
+++ lesson3/task32.js
@@ -19,46 +19,5 @@
  * @return {string}
  */
 const longestCommonPrefix = function (strs) {
-    if (strs.length <= 0) {
-        console.error("Error: strs.length < 0");
-        return null;
-    }
-
-    let maxSize = Number.MAX_SAFE_INTEGER;
-    let prefix = "";
-
-
-    // find max prefix length
-    for (let i = 0; i < strs.length; i++)
-        if (maxSize > strs[i].length) {
-            prefix = strs[i];
-            maxSize = strs[i].length;
-        }
-
-    //
-    for (let i = 0; i < strs.length; i++) {
-        if (prefix.length === 0)
-            break;
-        prefix = compare(strs[i], prefix);
-    }
-
-
-    // printing result
-    console.log(`${strs.toString()} \t=>\t Longest common prefix: ${prefix}`);
 
 };
-
-// symbol comparator
-function compare(string, prefix) {
-    for (let i = 0; i < prefix.length; i++)
-        // if characters ain't equal then decrease prefix's size by 1 and compare again
-        if (prefix.charAt(i) !== string.charAt(i))
-            return compare(prefix.slice(0, prefix.length - 2), string);
-
-    return prefix;
-}
-
-longestCommonPrefix(["test", "te2st", "testdfdfsf"])                    // Output: te
-longestCommonPrefix(["abc", "abcdfdfd", "sfdgfsg", "gdgfdgdf"])
-longestCommonPrefix(["flower", "flow", "flight"])
-longestCommonPrefix([])
