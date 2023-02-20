a94cea4b6aa1014ea1aa54c2f95a32e5a62b1cef
Artem Medvedev <a95.medvedev@gmail.com>
2023-02-20 14:20:15 +0200

    Task 33 solution

--- lesson3/task33.js
+++ lesson3/task33.js
@@ -40,3 +40,4 @@ const isIsomorphic = function(s, t) {
 console.log(isIsomorphic('egg', 'add')); // Output: true
 console.log(isIsomorphic('foo', 'bar')); // Output: false
 console.log(isIsomorphic('paper', 'title')); // Output: true
+console.log(isIsomorphic('1paper', 'title')); // Output: false
