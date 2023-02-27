01dc70f467458f1ebe2743d283f25d756259f0dd
DavidPark <Churkindava@gmail.com>
2023-02-27 14:22:51 +0200

    homework3

--- lesson3/task32.js
+++ lesson3/task32.js
@@ -18,6 +18,29 @@
  * @param {string[]} strs
  * @return {string}
  */
-const longestCommonPrefix = function(strs) {
+let array_words_right = ["flower", "flow", "flight"];
 
-};
+let array_words_not_right =  ["dog","racecar","car"];
+
+function prefix(array) {
+    if (!Array.isArray(array) || array.length === 0) return "";
+
+    let pref = "",
+        count_array_one = array.length,
+        count_array_two = array.length;
+    for (let idx = 0; idx < count_array_one; ++idx) {
+        let equals = true,
+            c = array[0][idx];
+        for (let j = 1; j < count_array_two; ++j) {
+            if (c !== array[j][idx]) {
+                equals = false;
+                break;
+            }
+        }
+        if (equals)
+            pref += c;
+    }
+    return pref;
+}
+console.log("Array1 = " + prefix(array_words_right));
+console.log("Array2 = " + prefix(array_words_not_right) + " empty");
--- lesson3/task33.js
+++ lesson3/task33.js
@@ -23,4 +23,25 @@
  * @param {string} t
  * @return {boolean}
  */
-const isIsomorphic = function(s, t) {};
+let stroka_right = "add";
+let temp_right = "egg";
+
+let stroka_not_right = "foo";
+let temp_not_right = "bar";
+const isIsomorphic = function(s, t) {
+
+    for (let idx = 0; idx < s.length; idx++) {
+
+        if (s.indexOf(s[idx], idx + 1) !== t.indexOf(t[idx], idx + 1)) {
+
+            return false;
+
+        }
+
+    }
+
+    return true;
+
+};
+console.log(stroka_right + " " + temp_right + " \n" + isIsomorphic(stroka_right,temp_right) + "\n");
+console.log(stroka_not_right + " " + temp_not_right + " \n" + isIsomorphic(stroka_not_right,temp_not_right));
