96c8d962ef06d67969f0be9bab8e889e3e161cd8
DavidPark <Churkindava@gmail.com>
2023-02-27 14:22:51 +0200

    homework3

--- lesson3/task31.js
+++ lesson3/task31.js
@@ -8,4 +8,19 @@
  * @param {string} s
  * @return {boolean}
  */
-const isPalindrome = function(s) {};
+let a = "ert43 er ertertdfg dfer";
+let s = "re4tt7er";
+let b = "race a car"
+a = a.replace(/[^a-z]/ig, "");
+s = s.replace(/[^a-z]/ig, "");
+b = b.replace(/[^a-z]/ig, "");
+
+console.log("s = " + s);
+console.log("a = " + a);
+console.log("b = " + b);
+function polindrom(str) {
+    return str === str.split('').reverse().join('');
+}
+console.log("\n" + b + " " + polindrom(b))
+console.log("\n" + s + " " + polindrom(s));
+console.log("\n" + a + " " + polindrom(a));
