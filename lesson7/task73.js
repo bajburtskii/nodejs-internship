4d32a7b5d49f2d36171a804f6182cbe2004b59f7
arcadij <arcadij@sportysystems.com>
2023-03-08 16:39:21 +0200

    lesson 7

---
+++ lesson7/task73.js
@@ -0,0 +1,26 @@
+/**
+ * Given a string array words, return the maximum value of length(word[i]) * length(word[j]) where the two words do not share common letters. If no such two words exist, return 0.
+ *
+ * @example
+ *    Input: words = ["abcw","baz","foo","bar","xtfn","abcdef"]
+ *    Output: 16
+ *    Explanation: The two words can be "abcw", "xtfn".
+ *
+ * @example
+ *    Input: words = ["a","ab","abc","d","cd","bcd","abcd"]
+ *    Output: 4
+ *    Explanation: The two words can be "ab", "cd".
+ *
+ * @example
+ *    Input: words = ["a","aa","aaa","aaaa"]
+ *    Output: 0
+ *    Explanation: No such pair of words.
+ */
+
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+const maxProduct = function(words) {
+
+};
